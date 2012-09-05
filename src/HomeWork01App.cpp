d7cb791c2983f92676cbad33f6aa80e5ddc10e3a
harmonbc <harmonbc@muohio.edu>
2012-09-04 23:03:57 -0400

    Got Something Working

--- src/HomeWork01App.cpp
+++ src/HomeWork01App.cpp
@@ -17,21 +17,25 @@ public:
 	void update();
 	void draw();
 	void prepareSettings(Settings* settings);
-	void fadingBackground(uint8_t* pixels);
 	void drawInvertSquares(uint8_t* pixels, int side);
 	void drawInvertCircle(uint8_t* pixels, int center_x, int center_y, int r);
 	void addTint(uint8_t* pixels, Color8u c);
 	void addBlur(uint8_t* pixels);
-	void spinningHands(uint8_t* pixels);
 	void drawLine(uint8_t* pixels,int x1, int x2, int y1, int y2, int width);
-	void setPixel(uint8_t* pixels,int x, int y, int width);
+	void plot(uint8_t* pixels,int x, int y, int width);
 
 private:
 	Surface* mySurface_;
 	int frame_number_;
 	int hand_position;
 	int kCircleRadius_;
-	bool rotate_clockwise_;
+	bool firstClick;
+	bool secondClick;
+	int xOne;
+	int xTwo;
+	int yOne;
+	int yTwo;
+
 	double rotate_pos_;
 };
 static const int kAppWidth=800;
@@ -49,12 +53,28 @@ void HomeWork01App::setup()
 	mySurface_ = new Surface(kTextureSize,kTextureSize,false);
 	frame_number_ = 0;
 	kCircleRadius_ = 200;
-	rotate_pos_ = 1;
+	firstClick = false;
+	secondClick = false;
 }
 
 void HomeWork01App::mouseDown( MouseEvent event )
 {
-	console() << event.getX() << ", " << event.getY() <<endl;
+	if(firstClick && secondClick)
+	{
+		firstClick = false;
+		secondClick = false;
+	}
+	if(!firstClick)
+	{
+		firstClick = true;
+		xOne = event.getX();
+		yOne = event.getY();
+	}else
+	{
+		secondClick = true;
+		xTwo = event.getX();
+		yTwo = event.getY();
+	}
 }
 
 void HomeWork01App::drawInvertCircle(uint8_t* pixels, int center_x, int center_y, int r)
@@ -120,105 +140,64 @@ void HomeWork01App::drawInvertSquares(uint8_t* pixels, int side)
 	}
 }
 
-/**
-*Gradient A.4
-**/
-void HomeWork01App::fadingBackground(uint8_t* pixels)
-{
-	int32_t something = (*mySurface_).getWidth()*(*mySurface_).getHeight();
-	int y,x;
 
-	for(y=0; y<kAppHeight; y++)
-	{
-		for(x=0; x<kAppWidth; x++)
-		{
-
-		}
-	}
-}
-
-void HomeWork01App::spinningHands(uint8_t* pixels)
-{
-	int spinnerLength = kCircleRadius_;
-
-	time_t now = time(0);
-
-	tm *ltm = localtime(&now);
-
-	rotate_pos_ = (ltm->tm_sec/30.0)+.25;
-
-	console() << rotate_pos_ << endl;
-	int startx = kAppWidth/2;
-	int starty = kAppHeight/2;
-
-	for(int y=starty-spinnerLength; y<starty+spinnerLength; y++)
-	{
-		for(int x=startx-spinnerLength; x<startx+spinnerLength; x++)
-		{
-			double denominator = x-startx;
-			double currentSlope = y-starty;
-
-			int dist = (int)sqrt((double)(denominator*denominator+currentSlope*currentSlope));
-
-			if(dist<=spinnerLength)
-			{
-				currentSlope =  currentSlope/denominator;
-
-				if(abs(currentSlope-rotate_pos_)<.1)
-				{
-					int curBlock = 3*(x + y*kTextureSize);
-					Color8u c = Color8u(255,255,255);
-
-					pixels[curBlock] = c.r;
-					pixels[curBlock+1] = c.g;
-					pixels[curBlock+2] = c.b;
-				}
-
-			}
-		}
-	}
-}
 /**
 *Retrived sudo code from wiki - http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 **/
 void HomeWork01App::drawLine(uint8_t* pixels, int x0, int x1, int y0, int y1, int width)
 {
+	bool steep = abs(y1-y0) > abs(x1-x0);
 
-	int dx = abs(x1-x0);
-	int dy = abs(y1-y0);
-	int sx = (x0 < x1) ? 1 : -1;
-	int sy = (y0 < y1) ? 1 : -1;
-	int err = dx-dy;
-
-	for(int x = x0; x<x1; x++)
+	if(steep)
 	{
-		setPixel(x0,y0,width);
+		swap(x0, y0);
+		swap(x1, y1);
+	}
 
-		if ((x0 == x1)&&(y0 = y1))
-			break;
+	if(x0 > x1)
+	{
+		swap(x0, x1);
+		swap(y0, y1);
+	}
 
-		int e2 = 2*err;
+	int dx = x1 - x0;
+	int dy = abs(y1-y0);
+	double error = dx/2;
 
-		if (e2 > -dy)
+	int ystep;
+
+	int y = y0;
+
+	ystep = (y0<y1) ? 1 : -1;
+
+	for(int x=x0; x<x1; x++)
+	{
+		int dist = (int)sqrt((double)((x0-x)*(x0-x)+(y0-y)*(y0-y)));
+
+
+		(steep) ? plot(pixels, y,x, 5) : plot(pixels, x,y, 5);
+		error = error - dy;
+
+		if(error<0)
 		{
-			err = err - dy;
-			x0 = x0 + sx;
-		}
-		if (e2 <  dx)
-		{
-			err = err + dx;
-			y0 = y0 + sy;
+			y = y + ystep;
+			error = error + dx;
 		}
 	}
 
-
 }
-void setPixel(uint8_t* pixels, int x, int y, int width)
+
+void HomeWork01App::plot(uint8_t* pixels, int x, int y, int width)
 {
 	int halfWidth = (width<2)?1:width/2;
-	for(int i=x-halfWidth;i<x+halfWidth;x++)
+
+	console() << "PLOTTING " << x << " " << y << " " << width << endl;
+	int op1 = (width<3) ? 1 : (x-width);
+	int op2 = (width<3) ? 1 : (x+width);
+
+	for( x = op1 ; x < op2; x++)
 	{
-		int offset = 3*(i+y*kTextureSize);
+		int offset = 3*(x+y*kTextureSize);
 		pixels[offset] = 0;
 		pixels[offset+1] = 0;
 		pixels[offset+2] = 0;
@@ -231,11 +210,15 @@ void HomeWork01App::update()
 	Color8u fill1 = Color8u(128,128,192);
 	Color8u tint = Color8u(255,0,0);
 
-	fadingBackground(dataArray);
 	drawInvertSquares(dataArray, kCircleRadius_);
 	drawInvertCircle(dataArray, kAppWidth/2, kAppHeight/2, kCircleRadius_);
 	addTint(dataArray, tint);
-	spinningHands(dataArray);
+
+	if(firstClick&&secondClick)
+	{
+		drawLine(dataArray,xOne, xTwo, yOne, yTwo, 5);
+	}
+
 	frame_number_++;
 }
 
