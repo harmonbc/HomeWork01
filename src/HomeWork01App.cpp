5af2f42690d8eedab02671ae0148211f33560fc9
Brandon Harmon <harmonbc@muohio.edu>
2012-09-05 22:11:52 -0300

    Added Header

--- src/HomeWork01App.cpp
+++ src/HomeWork01App.cpp
@@ -1,3 +1,15 @@
+/**
+* @file CatPictureApp.cpp
+* A simple app that changes the color of the background
+*
+* @author Brandon Harmon
+* @date 2012-09-05
+*
+* @note This file is (c) 2012. It is licensed under the
+* CC BY 3.0 license (http://creativecommons.org/licenses/by/3.0/),
+* which means you are free to use, share, and remix it as long as you
+* give attribution. Commercial uses are allowed.
+**/
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
